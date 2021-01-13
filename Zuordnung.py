6cf292a449b3479b230880b0bc593e327f54b09e
Jonas <jonas.h.weiss@gmail.com>
2021-01-13 20:39:26 +0100

    fix some PyCharm formatting issues.

--- Zuordnung.py
+++ Zuordnung.py
@@ -1,8 +1,9 @@
 import random
 
+
 class Messdiener:
-    def __init__(self,name):
-    #MD is used as an abbreviation of Messdiener
+    def __init__(self, name):
+        # MD is used as an abbreviation of Messdiener
         self.name = name
 
         pass
@@ -15,7 +16,6 @@ class Messe:
         pass
 
 
-
 MD1 = Messdiener("Messdiener1")
 MD2 = Messdiener("Messdiener2")
 MD3 = Messdiener("Messdiener3")
@@ -25,13 +25,15 @@ MD6 = Messdiener("Messdiener6")
 MD7 = Messdiener("Messdiener7")
 MD8 = Messdiener("Messdiener8")
 MD9 = Messdiener("Messdiener9")
-ListeMD=[MD1, MD2, MD3, MD4, MD5, MD6, MD7, MD8, MD9]
-Messe1= Messe(4)
+
+ListeMD = [MD1, MD2, MD3, MD4, MD5, MD6, MD7, MD8, MD9]
+Messe1 = Messe(4)
 
 
 def allocation_md(messe_aktuell):
-    for x in range (0,messe_aktuell.Anzahl):
+    for x in range(0, messe_aktuell.Anzahl):
         random_md = random.choice(ListeMD)
         print(random_md.name)
+
 
 allocation_md(Messe1)
