41a0ec594175a072816b926a3fdcb9b80631f8ac
Jonas <jonas.h.weiss@gmail.com>
2021-01-14 13:23:07 +0100

    update the availability and fix it upon creation of ChurchServer, through the function create_availability. ChurchServer objects are created through a for loop.

--- Zuordnung.py
+++ Zuordnung.py
@@ -5,10 +5,15 @@ class ChurchServers:
     def __init__(self, name):
         # MD is used as an abbreviation of ChurchServers
         self.name = name
+        # Shows if someone is already in the current Messe
         self.is_allocated = True
+        # Shows if someone has excused themselves
         self.is_available = False
+        # Shows if someone is able to serve at Church
         self.is_advanced = True
-        self.grade = "Child"
+        # Grade is simplified into "Child" and "GroupLeader"
+        # "Null" is a String to show the grade did not work
+        self.grade = "Null"
 
 
 class ChurchService:
@@ -22,26 +27,32 @@ class ChurchService:
         pass
 
 
-def is_available(church_server):
-    if random.randint(0, 8) == 1:
+def create_availability(church_server):
+    # Placeholder to create a state to check if someone is available
+    if random.randint(0, 6) == 1:
         church_server.is_available = False
+        # print(church_server.name)
     else:
         church_server.is_available = True
+
+
+def is_available():
+    # Placeholder for later check
+    pass
 
 
 def is_assigned(check_church_service, church_server):
     # the Function checks if a ChurchServer is already assigned to the Service
     if check_church_service.ListServingMD.count(church_server.name) == 0:
         church_server.is_allocated = False
-        print(church_server.name + "is not available")
     else:
         church_server.is_allocated = True
 
 
 def allocation_md(current_church_service):
     while len(current_church_service.ListServingMD) < current_church_service.count:
-        random_md = random.choice(ListMD)
-        is_available(random_md)
+        random_md = random.choice(List_MD)
+        is_available()
         if random_md.is_available:
             is_assigned(current_church_service, random_md)
             if not random_md.is_allocated:
@@ -54,21 +65,25 @@ def allocation_md(current_church_service):
         pass
 
 
-MD1 = ChurchServers("Messdiener1")
-MD1.is_available = False
-MD2 = ChurchServers("Messdiener2")
-MD2.is_available = False
-MD3 = ChurchServers("Messdiener3")
-MD4 = ChurchServers("Messdiener4")
-MD5 = ChurchServers("Messdiener5")
-MD6 = ChurchServers("Messdiener6")
-MD7 = ChurchServers("Messdiener7")
-MD8 = ChurchServers("Messdiener8")
-MD9 = ChurchServers("Messdiener9")
+List_MD = []
 
-ListMD = [MD1, MD2, MD3, MD4, MD5, MD6, MD7, MD8, MD9]
-Messe1 = ChurchService(7)
-Messe2 = ChurchService(3)
+
+# Generation of ChurchServers
+for number_id in range(0, 20):
+    List_MD.append(ChurchServers("Messdiener" + str(number_id)))
+    create_availability(List_MD[number_id])
+
+Messe1 = ChurchService(8)
+Messe2 = ChurchService(8)
+Messe3 = ChurchService(8)
+
 allocation_md(Messe1)
+allocation_md(Messe2)
+allocation_md(Messe3)
+
+print("Messe1")
 print(Messe1.ListServingMD)
+print("Messe2")
 print(Messe2.ListServingMD)
+print("Messe3")
+print(Messe3.ListServingMD)
