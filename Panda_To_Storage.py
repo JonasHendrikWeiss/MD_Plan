9bfb65c217bb40301eb9e3a7982d852c7eb25d5c
Jonas <jonas.h.weiss@gmail.com>
2022-04-08 19:22:39 +0200

    move the code to handle generating TimeSpan Objects out of the saved dictionaries to a separate function

--- Panda_To_Storage.py
+++ Panda_To_Storage.py
@@ -20,15 +20,7 @@ def import_churchservers_from_dataframe(pdataframe, server_list):
     for server in range(0, pdataframe.size):
         temp_list = []  # creates a temporary list for each server
         if pdataframe.at[0, server]["unavailable"]:  # only uses ChurchServers which unavailable lists are not empty
-            for selected_entry in range(len(pdataframe.at[0, server]["unavailable"])):
-                # stamp_start is the timestamp of the start_date
-                stamp_start = int(pdataframe.at[0, server]["unavailable"][selected_entry]["start_date"])
-                # converts the timestamp back into a datetime.date
-                date_start = datetime.fromtimestamp(stamp_start / 1e3).date()
-                # stamp_end is the timestamp of the end_date
-                stamp_end = int(pdataframe.at[0, server]["unavailable"][selected_entry]["end_date"])
-                date_end = datetime.fromtimestamp(stamp_end / 1e3).date()
-                temp_list.append(TimeSpan(date_start, date_end)) # creates a TimeSpan Object out of the timestamps
+            temp_list = load_TimeSpan(pdataframe.at[0, server]["unavailable"])
 
         current_cserver = ChurchServers(lastname=pdataframe.at[0, server]["lastname"],
                                         firstname=pdataframe.at[0, server]["firstname"],
@@ -43,14 +35,18 @@ def list_to_json(list, dir_path=os.path.dirname(os.path.realpath(__file__)),
     pandas.DataFrame([list]).to_json(path_or_buf=f"{dir_path}/{filename}",)
     # if it crashed at [] around list. Removed it because the size of the data was increasing
 
-def load_TimeSpan(dictionary, list): # loads TimeSpan Objects to a list
-    for selected_entry in range(len(dictionary)):
+
+def load_TimeSpan(unavailable_list):
+    # Takes the list of dictionaries that are generated when TimeSpan Objects are Saved and returns a list of TimeSpan
+    # objects
+    return_list = []
+    for selected_entry in range(len(unavailable_list)):
         # stamp_start is the timestamp of the start_date
-        stamp_start = int(dictionary["start_date"])
+        stamp_start = int(unavailable_list[selected_entry]["start_date"])
         # converts the timestamp back into a datetime.date
         date_start = datetime.fromtimestamp(stamp_start / 1e3).date()
         # stamp_end is the timestamp of the end_date
-        stamp_end = int(dictionary[selected_entry]["end_date"])
+        stamp_end = int(unavailable_list[selected_entry]["end_date"])
         date_end = datetime.fromtimestamp(stamp_end / 1e3).date()
-        list.append(TimeSpan(date_start, date_end)) # creates a TimeSpan Object out of the timestamps
-    return list
+        return_list.append(TimeSpan(date_start, date_end)) # creates a TimeSpan Object out of the timestamps
+    return return_list
